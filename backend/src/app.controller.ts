64a6f8c56c92ce7d010c12476492dfca94a16733
Ajinkya Kadhe <ajinkya.kadhe@claimgenius.com>
2023-07-16 19:51:36 +0530

    video file converter added

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,8 +1,10 @@
 import { Body, Controller, Get, HttpCode } from '@nestjs/common';
 import { Delete, Param, Post, Put, Query } from '@nestjs/common/decorators';
+import { resolve, join } from 'path';
 import { AppService } from './app.service';
 import { GetEmployeeQuerytDto, SubmitDetailsDto } from './employee.dto';
-
+import * as ffmpeg from 'fluent-ffmpeg';
+ffmpeg.setFfmpegPath('ffmpeg.exe');
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -33,7 +35,7 @@ export class AppController {
   async update(
     @Param('id') id: number,
     @Body() updateDetails: SubmitDetailsDto,
-  ){
+  ) {
     const result = await this.appService.edit(id, updateDetails);
     return {
       success: true,
@@ -50,5 +52,34 @@ export class AppController {
       data: result,
       message: 'Data removed sucessfully',
     };
+  }
+
+  @HttpCode(200)
+  @Get('convert')
+  async convertVideo() {
+    const inputPath = resolve(
+      __dirname,
+      '../Merimen/UNZIP/14577968/Claimant_202302200062_1.mp4',
+    );
+    const outputPath = join(
+      __dirname,
+      '../Merimen/UNZIP/14577968/Converted_Claimant_202302200062_1.mp4',
+    );
+
+    return new Promise((resolve, reject) => {
+      ffmpeg(inputPath)
+        .output(outputPath)
+        .on('end', () => {
+          console.log('Conversion finished!');
+          resolve({
+            filePath: outputPath,
+            contentType: 'video/mp4',
+          });
+          reject((err) => {
+            return err;
+          });
+        })
+        .run();
+    });
   }
 }
